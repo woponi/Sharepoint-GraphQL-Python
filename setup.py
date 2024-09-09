cd7f86ead1ef555b5d91ef9f918b10ea2ffa4adf
pong <woponi@gmail.com>
2024-09-10 03:41:04 +0800

    add comment to version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sharepoint_graphql',
-    version='0.1.5', #Remember to update version
+    version='0.1.5', #Remember to update version test
     description='This Python utility enables users to interact with SharePoint sites via Microsoft Graph API, facilitating tasks such as listing, downloading, uploading, moving, and deleting files.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
